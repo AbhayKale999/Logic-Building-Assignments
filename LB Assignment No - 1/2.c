ca381b926e25b80922e5dd278e6b7455169d8ddc
AbhayKale999 <AbhayKale3097@gmail.com>
2025-05-10 19:16:24 +0530

    Add files via upload

---
+++ LB Assignment No - 1/2.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+void Display()
+{
+    int iCnt = 5;
+    for (int i = 0; i < iCnt; i++)
+    {
+        printf("Marvellous \n");
+    }
+    
+}
+int main()
+{
+    Display();
+
+    return 0;
+
+}
